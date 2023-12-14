04bd7a4fd293dfe3b5278661a1456a1bee5291a2
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-12-14 20:17:38 +0530

    Adds the URL functions

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+   
+function openInNewTab(url) { // when a user click the URL path and will open a new tab.
+  var win = window.open(url, '_blank');
+  win.focus();
+  } 
